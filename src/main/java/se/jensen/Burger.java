409505b4f9fd555d36494eae64e52453f404adbc
Rufus100Procent <Maxamadabdulahist@outlook.com>
2023-01-17 15:34:47 +0100

    "changess"

--- src/main/java/se/jensen/Burger.java
+++ src/main/java/se/jensen/Burger.java
@@ -5,6 +5,7 @@ public class Burger  extends Iteam{
     private Iteam topping1;
     private Iteam topping2;
     private Iteam dtopping2;
+    private Iteam tdtopping2;
 
     public Burger(String spacialburger, int price) {
         super();
